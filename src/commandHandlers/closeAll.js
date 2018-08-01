41dd67f8a3a54f644853b15156fc027cf893b566
Artur Diniz Adam <artur.daz@gmail.com>
2018-08-01 20:37:13 -0300

    Removing closeAllEditors command when closing window fixes #11

--- src/commandHandlers/closeAll.js
+++ src/commandHandlers/closeAll.js
@@ -3,6 +3,5 @@ const vscode = require('vscode')
 const QuitMenu = require('../QuitMenu')
 
 module.exports = function closeAll(textEditor){
-    vscode.commands.executeCommand('workbench.action.closeAllEditors')
     QuitMenu.showFocusingCloseWindow()
 }
